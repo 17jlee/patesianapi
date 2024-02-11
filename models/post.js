3b61595ab59357847bffa10257afe93f7b773f91
17jlee <17jlee@patesgs.org>
2024-02-11 12:44:30 +0000

    disabled titles

--- models/post.js
+++ models/post.js
@@ -11,7 +11,7 @@ const postSchema = new mongoose.Schema({
     },
     title: {
         type: String,
-        required: true
+        required: false
     },
     content: {
         type: String,
