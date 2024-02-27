42d96f2a605c464ad1640cedf967805b1490cf87
17jlee <17jlee@patesgs.org>
2024-02-27 12:07:39 +0000

    large

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+
+app.use(express.json({ limit: '50mb' }));
+
 mongoose.connect('mongodb://localhost/posts')
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
